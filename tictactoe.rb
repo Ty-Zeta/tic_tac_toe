0b19f5a507c4390bd913be4e733a3f46f5a28090
Ty Cook <tycook@Tys-MacBook-Pro.local>
2017-09-27 12:07:14 -0400

    tested for all possible wins & for no possible winner

--- tictactoe.rb
+++ tictactoe.rb
@@ -30,7 +30,21 @@ def test_for_full_board(board)
 end
 
 def win_condition(board)
-    if board[0] == "x" && board[3] == "x" && board[6] == "x"
+    if board[0] == "x" && board[3] == "x" && board[6] == "x"  || board[0] == "o" && board[3] == "o" && board[6] == "o"
+        true
+    elsif board[1] == "x" && board[4] == "x" && board[7] == "x" || board[1] == "o" && board[4] == "o" && board[7] == "o"
+        true
+    elsif board[2] == "x" && board[5] == "x" && board[8] == "x"  || board[2] == "o" && board[5] == "o" && board[8] == "o"
+        true
+    elsif board[0] == "x" && board[1] == "x" && board[2] == "x"  || board[0] == "o" && board[1] == "o" && board[2] == "o"
+        true
+    elsif board[3] == "x" && board[4] == "x" && board[5] == "x"  || board[3] == "o" && board[4] == "o" && board[5] == "o"
+        true
+    elsif board[6] == "x" && board[7] == "x" && board[8] == "x"  || board[6] == "o" && board[7] == "o" && board[8] == "o"
+        true
+    elsif board[0] == "x" && board[4] == "x" && board[8] == "x"  || board[0] == "o" && board[4] == "o" && board[8] == "o"
+        true
+    elsif board[2] == "x" && board[4] == "x" && board[6] == "x"  || board[2] == "o" && board[4] == "o" && board[6] == "o"
         true
     else
         false
--- tictactoe_test.rb
+++ tictactoe_test.rb
@@ -38,8 +38,48 @@ class TestTicTacToe < Minitest::Test
         assert_equal(true, test_for_full_board(board))
     end
 
-    def test_win
+    def test_win_147
         board = ['x', 'o', '3', 'x', 'o', '6', 'x', '8', '9']
         assert_equal(true, win_condition(board))
+    end
+
+    def test_win_258
+        board = ['1','x','3','4','x','6','7','x','9']
+        assert_equal(true, win_condition(board))
+    end
+
+    def test_win_369
+        board = ['1','2','x','4','5','x','7','8','x']
+        assert_equal(true, win_condition(board))
+    end
+
+    def test_win_159
+        board = ['x','2','3','4','x','6','7','8','x']
+        assert_equal(true, win_condition(board))
+    end
+
+    def test_win_357
+        board = ['1','2','x','4','x','6','x','8','9']
+        assert_equal(true, win_condition(board))
+    end
+
+    def test_win_123
+        board = ['o','o','o','4','5','6','7','8','9']
+        assert_equal(true, win_condition(board))
+    end
+
+    def test_win_456
+        board = ['1','2','3','o','o','o','7','8','9']
+        assert_equal(true, win_condition(board))
+    end
+
+    def test_win_789
+        board = ['1','2','3','4','5','6','o','o','o']
+        assert_equal(true, win_condition(board))
+    end
+
+    def test_fail
+        board = ['x','x','o','o','o','x','x','o','x']
+        assert_equal(false, win_condition(board))
     end
 end
