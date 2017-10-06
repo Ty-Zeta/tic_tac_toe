b3a706358f662cd819fcec75a3e70b7e882844f9
Ty Cook <tycook@Tys-MacBook-Pro.local>
2017-10-06 13:18:57 -0400

    added more testing

--- test_impossible.rb
+++ test_impossible.rb
@@ -48,4 +48,24 @@ class TestImpossible < Minitest::Test
         assert_equal(0, impossible_variable.get_move(["", "", "", "", "O", "", "", "", ""]))
     end
 
+    def test_blocks_fork
+		impossible_variable = Impossible.new('X')
+		assert_equal(4, impossible_variable.get_move(['O', 'X', '', 'X', '', '', 'O', '', '']))
+    end
+
+    def test_O_fork_block2
+		impossible_variable = Impossible.new('O')
+		assert_equal(2, impossible_variable.get_move(['O', '', '', '', '', '', 'X', 'O', 'X']))
+    end
+
+    def test_O_blocks_X_corners
+		impossible_variable = Impossible.new("O")
+        assert_equal(3, impossible_variable.get_move(['', '', 'X', '', 'O', '', 'X', '', '']))
+    end
+
+    def test_O_takes_top_side
+		impossible_variable = Impossible.new("O")
+        assert_equal(1, impossible_variable.get_move(["X", '', "X", '', "O", '', '', "O", "X"]))
+    end
+
 end
